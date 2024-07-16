e212aa20fb239d70f1743824015eeac50fb52bc4
Maks Plevako <maksplevako@gmail.com>
2024-07-16 16:09:33 +0300

    start adaptive for tablet

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo_mobile from '../img/Logo_Mobile.png'
+import logo_tablet from '../img/logo.png'
 import '../style/app.css'
 import Home from './home'
 import { useSelector, useDispatch } from 'react-redux'
@@ -13,9 +14,15 @@ export default function Header() {
 		<div className='header mb-6'>
 			<header>
 				<div className='flex flex-grid justify-around items-center py-2 '>
-					<div>
-						<img src={logo_mobile} alt='logo mobile' />
+					<div className='lg:basis-1/2 pl-9'>
+						<img src={logo_mobile} alt='logo mobile' className='lg:hidden' />
+						<img
+							src={logo_tablet}
+							alt='logo tablet'
+							className='hidden lg:block'
+						/>
 					</div>
+					<div>UA</div>
 					<button className='border-2 rounded-xl border-[#EF3959] bg-[#EF3959] px-[25px] py-[15px] text-white text-xl active:bg-white active:text-[#EF3959]'>
 						Допомогти
 					</button>
@@ -57,7 +64,7 @@ export default function Header() {
 				{nav && (
 					<div className='xl:hidden w-full h-screen'>
 						<div className='border border-black w-11/12 mx-auto' />
-						<div className='text-center'>
+						<div className='text-center text-[#202053] font-medium lg:text-xl'>
 							<ul>
 								<li className='my-6'>
 									<button className='border border-white py-2 px-3'>
--- src/components/home.js
+++ src/components/home.js
@@ -1,29 +1,37 @@
 import React from 'react'
 import dog1 from '../img/dog_mobile.png'
 import decor from '../img/decor.png'
+import dogTablet from '../img/dog_tablet.png'
+import decorTablet from '../img/decor_tablet.png'
 
 export default function Home() {
 	return (
-		<div className='py-2 home'>
-			<div className='mb-6 text-4xl text-[#202053] font-semibold w-11/12 mx-auto'>
-				Цим оченятам дуже потрібні любов і турбота
+		<div className='py-10 home lg:flex lg:flex-row lg:mx-auto lg:w-full'>
+			<div>
+				<div className='mb-6 text-4xl text-[#202053] font-semibold w-11/12 sm:max:lg:mx-auto lg:text-6xl lg:ml-8'>
+					Цим оченятам дуже потрібні любов і турбота
+				</div>
+				<div className='text-xl text-[#404040] w-11/12 sm:max-lg:mx-auto lg:text-2xl lg:mb-20 lg:w-8/12 lg:text-left lg:ml-8'>
+					Вірний друг радітиме тобі завжди. Йому потрібно дуже мало - теплий дім
+					та дбайливий господар
+				</div>
+				<div className='w-full my-6 relative lg:hidden'>
+					<img src={decor} alt='decor' className='relative mx-auto' />
+					<img
+						src={dog1}
+						alt='dog1'
+						className='absolute mx-auto top-0 left-1/2 -translate-x-1/2'
+					/>
+				</div>
+				<div className='w-full text-center mt-6 lg:w-auto lg:text-left lg:ml-8'>
+					<button className='text-4xl border-2 rounded-2xl w-11/12 py-3 border-[#EF3959] bg-[#EF3959] text-white rounded-bl-none  active:bg-white active:text-[#EF3959] lg:w-3/5 lg:py-8 lg:font-semibold'>
+						Обрати
+					</button>
+				</div>
 			</div>
-			<div className='text-xl text-[#404040] w-11/12 mx-auto'>
-				Вірний друг радітиме тобі завжди. Йому потрібно дуже мало - теплий дім
-				та дбайливий господар
-			</div>
-			<div className='w-full my-6 relative'>
-				<img src={decor} alt='decor' className='relative mx-auto' />
-				<img
-					src={dog1}
-					alt='dog1'
-					className='absolute mx-auto top-0 left-1/2 -translate-x-1/2'
-				/>
-			</div>
-			<div className='w-full text-center mt-6'>
-				<button className='text-4xl border-2 rounded-2xl w-11/12 py-3 border-[#EF3959] bg-[#EF3959] text-white rounded-bl-none  active:bg-white active:text-[#EF3959]'>
-					Обрати
-				</button>
+			<div className='w-full relative hidden lg:block'>
+				<img src={decorTablet} alt='decor' className='relative' />
+				<img src={dogTablet} alt='dog1' className='absolute top-0' />
 			</div>
 		</div>
 	)
